6cf601a08c6cc442dad1e2d8f325bdd71c6ceefd
Abiona Israel <israelabiona44@gmail.com>
2026-05-29 12:07:59 +0100

    feat: add DigitalClock component and update styles for background and layout

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+
+function DigitalClock(){
+
+  return(
+    <div className="container">
+
+      <div className="clock">
+        <span>00:00:00</span>
+        </div>
+
+    </div>
+  );
+
+}
+
+export default DigitalClock
